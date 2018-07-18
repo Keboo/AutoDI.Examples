7a1c5854bc3d6b7076c4efd837dbd1cb66e425ed
Kevin B <kitokeboo@gmail.com>
2018-07-18 10:23:16 -0700

    Nuget revs. Replaced on ASP.NET Core project with vunerabilities with a new one.

---
+++ Platforms/ASP.NetCore/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ASP.NetCore.Models;
+using AutoDI.Container.Examples;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+
+namespace ASP.NetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IQuoteService _service;
+
+        public HomeController(IQuoteService service)
+        {
+            _service = service ?? throw new ArgumentNullException(nameof(service));
+        }
+
+        public IActionResult Index()
+        {
+            return View(_service.GetQuotes());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Platforms/ASP.NetCore/Program.cs
@@ -0,0 +1,20 @@
+﻿using AutoDI.AspNetCore;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ASP.NetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseAutoDI()
+                .Build();
+    }
+}
